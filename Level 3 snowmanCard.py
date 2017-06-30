c687a8eb18e66e057d03394d8dfd888d731f5f2a
Sam Wong <sam.wong.uk@gmail.com>
2017-06-30 23:28:24 +0100

    Update Level 3 snowmanCard.py

--- Level 3 snowmanCard.py
+++ Level 3 snowmanCard.py
@@ -1,10 +1,9 @@
-
 import turtle
 from colors import *
 from shapes import *
 from random import randint
 
-bgColor = colors['lightblue']
+bgColor = colors['white']
 
 # Define global scoped variables
 currentY = -200  #trunk Y position
@@ -14,55 +13,22 @@ trunkHeight = 80  #trunk height
 # Define a turtle screen object
 window = turtle.Screen()
 window.bgcolor(bgColor)
-window.title('Christmas Tree')
+window.title('Snowman')
 
 # Define a turtle pen object
 myPen = turtle.Turtle()
 myPen.speed(10)      
 
-def showCardName():
-    myPen.penup
-    myPen.color(bgColor)
-    myPen.goto(0,-300)
-    myPen.color(colors['red'])
-    myPen.write('Merry Christmas', align='center', font=('Arial',30,('bold')))
-    myPen.hideturtle()
+def drawBackground():
+    drawCircle(myPen, colors['lightblue'], 0, -300, 300)
     return
 
-def drawRectangle(turtle, color, x, y, width, height):
-    turtle.penup()
-    turtle.goto(x, y)
-    turtle.color(color)
-    turtle.fillcolor(color)
-    turtle.setheading(0)
-    turtle.pendown()
-    turtle.begin_fill()
-    for i in range(2):
-        turtle.forward(width)
-        turtle.left(90)
-        turtle.forward(height)
-        turtle.left(90)
-    turtle.end_fill()
-    turtle.setheading(0)
-    turtle.hideturtle()
-    return   
+def drawSnowflakes():
+    x = randint(30, 60)
+    y = randint(30, 60)
     
-def drawTrunk():
-    drawRectangle(myPen, colors['brown'], -20, currentY, trunkWidth, trunkHeight)
+    return
 
-def drawTree():
-    drawTrunk()
-    y = currentY + trunkHeight  
-    treeWidth = 350
-    for i in range(0, 9):
-        treeWidth = treeWidth - randint(30, 41)
-        treeHeight = randint(25,50)
-        x = 0 - treeWidth / 2
-        drawRectangle(myPen, colors['green'], x, y, treeWidth, treeHeight)
-        y = y + treeHeight
-    drawStar (myPen, colors['yellow'], 0, y+40, 100)  
-    
-showCardName()
-drawTree()
+drawBackground()
 window.mainloop()
 
